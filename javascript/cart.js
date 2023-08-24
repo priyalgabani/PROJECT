03d57f0b5631c0cdd51dca92042183080c417042
priyalgabani <priyalgabani2004@gmail.com>
2023-08-24 13:08:04 +0530

    cart

--- javascript/cart.js
+++ javascript/cart.js
@@ -6,3 +6,77 @@ fetch(`http://localhost:3000/cart`)
             .then((data) => {
                 console.log(data)
             })
+
+            let cart = JSON.parse(localStorage.getItem("cart")) || [];
+            let totalprice = 0;
+            cart.map((ele) => {
+              totalprice += ele.price * ele.qty * 10;
+            });
+            
+            
+            let show = (cart) => {
+            
+              cart.map((ele, index) => {
+            
+                let maindiv = document.createElement("div");
+            
+                let imgdiv = document.createElement("div");
+                let image = document.createElement("img");
+                image.src = ele.image;
+            
+                let texts = document.createElement("div");
+            
+                let title = document.createElement("h3");
+                title.innerHTML = ele.title;
+            
+                let minus = document.createElement("button");
+                minus.innerHTML = "-";
+            
+                let price1 = document.createElement("p");
+                price1.innerHTML = `${ele.price * 10}$`;
+            
+                let plusminse = document.createElement("div");
+            
+                let num = document.createElement("button");
+                num.innerHTML = ele.qty;
+            
+                let plus = document.createElement("button");
+                plus.innerHTML = "+";
+            
+                imgdiv.append(image);
+                texts.append(title, price1);
+                plusminse.append(minus, num, plus);
+                maindiv.append(imgdiv, texts, plusminus);
+                document.getElementById("cartpage").append(maindiv);
+            
+                price1.setAttribute("class", "price1");
+                maindiv.setAttribute("class", "maindiv");
+                imgdiv.setAttribute("class", "imgdiv");
+                texts.setAttribute("class", "texts");
+                plusminus.setAttribute("class", "plusminus");
+            
+                minus.addEventListener("click", () => {
+                  console.log(ele);
+                  let qty = cart[index].qty;
+                  if (qty == 1) {
+                    cart.splice(index, 1);
+                    localStorage.setItem("cart", JSON.stringify(cart));
+            
+                    console.log(cart);
+                    window.location.reload();
+                  } else {
+                    cart[index].qty -= 1;
+                    localStorage.setItem("cart", JSON.stringify(cart));
+                    window.location.reload();
+                  }
+                });
+                plus.addEventListener("click", () => {
+                  console.log("test");
+                  cart[index].qty += 1;
+                  localStorage.setItem("cart", JSON.stringify(cart));
+                  window.location.reload();
+                });
+              });
+            };
+            show(cart);
+            
--- javascript/pages.js
+++ javascript/pages.js
@@ -1,2 +1,95 @@
 import Navbar from "../components/nav.js";
 document.getElementById('navbar').innerHTML=Navbar()
+
+const display =(data) =>{
+    data.map((products)=>{
+        let img = document.createElement('img');
+        img.src=products.image;
+        let title = document.createElement('h2');
+        title.innerHTML=products.title;
+        let price = document.createElement('p');
+        price.innerHTML=products.price;
+        let category = document.createElement('p');
+        category.innerHTML=products.category;
+        let rating = document.createElement('span');
+        rating.innerHTML=products.rating.rate;
+        if(products.rating.rate > 4){
+            rating.style.color="green"
+        }
+        else if((products.rating.rate < 4) && (products.rating.rate >= 3)){
+            rating.style.color="yellow"
+        }
+        else{
+            rating.style.color="red"
+
+        }
+        let btn = document.createElement("button");
+    btn.innerHTML = "BUY NOW";
+    btn.addEventListener("click", () => {
+      let loggedIn = localStorage.getItem("loggedIn");
+      if (loggedIn) {
+        fetch(`http://localhost:3000/cart?id=${products.id}`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.length > 0) {
+              alert("products was sucess cart");
+              fetch(`http://localhost:3000/cart/${products.id}`, {
+                method: "PATCH",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({ qty: data[0].qty + 1 }),
+              });
+            } else {
+              console.log("product", products);
+              fetch("http://localhost:3000/cart", {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({ ...products, qty: 1 }),
+              });
+            }
+          });
+        } else {
+            alert("you have to login first");
+            setTimeout(() => {
+              window.location.href = "/pages/login.html";
+            }, 1000);
+          }
+        });
+        let div = document.createElement('div');
+        div.append(img,title,price,category,rating,btn)
+        document.getElementById("box2").append(div)
+    })
+}
+let products = [];
+
+document.getElementById("lth").addEventListener("click", () => {
+  products.sort((a, b) => a.price - b.price);
+  display(products);
+});
+
+document.getElementById("htl").addEventListener("click", () => {
+  products.sort((a, b) => b.price - a.price);
+  display(products);
+});
+
+document.getElementById("man").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "men's clothing");
+  display(temp);
+});
+
+document.getElementById("woman").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "women's clothing");
+  display(temp);
+});
+
+document.getElementById("jewelery").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "jewelery");
+  display(temp);
+});
+
+
+const get =()=>{
+    fetch("http://localhost:3000/products")
+    .then((response)=>response.json())
+    .then((response)=>display(response))
+}
+get()
